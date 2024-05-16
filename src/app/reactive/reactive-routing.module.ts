ba829a2c99462b7c7e2db81d7ed68232883aa69d
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-16 13:26:04 -0600

    Agregar las rutas principales del proyecto

--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,16 @@
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'basic', component: BasicPageComponent },
+  { path: 'dynamic', component: DynamicPageComponent },
+  { path: 'switches', component: SwitchesPageComponent },
+  { path: '**', redirectTo: 'basic' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
