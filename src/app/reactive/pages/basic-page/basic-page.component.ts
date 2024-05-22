86d74a0b061434b8b2e2ad1cb2c77dbbe9029900
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-22 11:21:26 -0600

    Update 22-02-2024

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
   //   price: new FormControl(0),
@@ -18,10 +24,14 @@ export class BasicPageComponent {
     inStorage: [0, [ Validators.required, Validators.min(0) ]]
    })
   constructor( private fb: FormBuilder ){}
+  ngOnInit(): void {
+    this.myForm.reset( rtx5090 );
+  }
 
 
   onSave():void {
     if ( this.myForm.invalid ) return;
     console.log( this.myForm.value );
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
