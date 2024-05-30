2123d15d53447fa99bbbaa119565fd4c6867c395
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-30 06:11:07 -0600

    Iniciando en Switches page component

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit{
+  public myForm: FormGroup = this.fb.group({
+    gender: [ 'M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.required ]
+  })
 
+  constructor( private fb: FormBuilder ){  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  onSave(){
+    if( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 }
